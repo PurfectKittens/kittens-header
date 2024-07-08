7dfa89afd3146b726b2b81e3009da81bc190267c
kittygirlyy <0xaa55@8086.re>
2024-07-08 20:08:14 +0200

    UPDATE : Username is stored now :3

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import * as vscode from 'vscode';
 import { generateComment } from './commentUtils';
 
+const USER_KEY = 'kittens-header.username';
+
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('kittens-header.initKitten', async () => {
     const editor = vscode.window.activeTextEditor;
@@ -8,9 +10,13 @@ export function activate(context: vscode.ExtensionContext) {
       return;
     }
 
-    const username = await vscode.window.showInputBox({ prompt: 'Entre ton nom ' });
+    let username = context.globalState.get<string>(USER_KEY);
     if (!username) {
-      return;
+      username = await vscode.window.showInputBox({ prompt: 'Entre ton nom ' });
+      if (!username) {
+        return;
+      }
+      await context.globalState.update(USER_KEY, username);
     }
 
     const document = editor.document;
@@ -35,4 +41,5 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() {
+}
